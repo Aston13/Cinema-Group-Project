bea84b2e3183e56e319b3a2f9bd69bd374b72e6e
Aston <astonoturner@gmail.com>
2019-03-20 15:42:28 +0100

    Remove old

--- To-Submit/EasyPHP_CS28/EasyPHP_v2/data/localweb/mice/application/views/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start(); 
-session_destroy(); // Destroying All Sessions 
-//header('Location:); // Redirecting To Home Page
-header('Location:http://localhost:8080/mice/index.php');
-?>
